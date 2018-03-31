d304fd74d6abea0ba0b81c5b5f0ea360513cebf3
Anton Mayatskiy <worknekki@gmail.com>
2018-03-31 19:16:30 +1000

    Переписал структуру кода исправил  замечания

--- js/main.js
+++ js/main.js
@@ -1,53 +1,51 @@
 (function () {
-  const Move = {
+  const KEY_CODE = {
     left: 37,
     right: 39
   };
 
-  class Screens {
+  const DIRECTION = {
+    next: `next`,
+    prev: `prev`
+  };
 
-    constructor() {
-      this._init();
+  // if (`content` in document.createElement(`template`)) {
+  //
+  // }
+
+  const screens = document.querySelectorAll(`template`);
+  const container = document.body.querySelector(`.central`);
+  const lastScreen = screens.length - 1;
+  let currentScreen = 0;
+
+  // Показывает экран в зависимости от переданного числа
+  const showScreen = function (current) {
+    const screen = screens[current];
+    const screenContent = screen.content.cloneNode(true);
+
+    container.innerHTML = ``;
+    container.appendChild(screenContent);
+  };
+
+  // Показываем начальный экран
+  showScreen(0);
+
+  // Переход по экранам
+  const navigate = function (dir) {
+    if (dir === DIRECTION.next) {
+      currentScreen = currentScreen < lastScreen ? currentScreen + 1 : 0;
+    } else {
+      currentScreen = currentScreen > 0 ? currentScreen - 1 : lastScreen;
     }
 
-    _init() {
-      this._currentScreen = 0;
-      this._screens = document.querySelectorAll(`template`);
-      this._container = document.body.querySelector(`.central`);
-      this._showScreen(this._currentScreen);
+    showScreen(currentScreen);
+  };
+
+  document.addEventListener(`keydown`, (e) => {
+    if ((e.keyCode === KEY_CODE.left) && e.altKey) {
+      navigate(DIRECTION.next);
+    } else if ((e.keyCode === KEY_CODE.right) && e.altKey) {
+      navigate(DIRECTION.prev);
     }
-
-    navigate(dir) {
-      const lastScreen = this._screens.length - 1;
-
-      if (dir === `next`) {
-        this._currentScreen = this._currentScreen < lastScreen ? this._currentScreen + 1 : 0;
-      } else {
-        this._currentScreen = this._currentScreen > 0 ? this._currentScreen - 1 : lastScreen;
-      }
-
-      this._showScreen(this._currentScreen);
-    }
-
-    _showScreen(current) {
-      const screen = this._screens[current];
-      const screenContent = screen.content.cloneNode(true);
-
-      this._container.innerHTML = ``;
-      this._container.appendChild(screenContent);
-
-    }
-  }
-
-  if (`content` in document.createElement(`template`)) {
-    const screens = new Screens();
-
-    document.addEventListener(`keydown`, (e) => {
-      if ((e.keyCode === Move.left) && e.altKey) {
-        screens.navigate(`prev`);
-      } else if ((e.keyCode === Move.right) && e.altKey) {
-        screens.navigate(`next`);
-      }
-    });
-  }
+  });
 })();
