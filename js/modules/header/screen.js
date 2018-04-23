9e919f2c2793f8d9088e48f01b34e7bfd3a81e75
Anton Mayatskiy <worknekki@gmail.com>
2018-04-23 17:09:29 +1000

    Добавил условие в headerScreen и добавил мигание для таймера

--- js/modules/header/screen.js
+++ js/modules/header/screen.js
@@ -2,10 +2,20 @@ import HeaderView from './view';
 import showIntro from '../intro/screen';
 
 export default (state) => {
+  console.log(state);
   const headerView = new HeaderView(state);
 
   headerView.goBack = () => {
-    showIntro();
+    if (!state) {
+      showIntro();
+      return;
+    }
+
+    const back = window.confirm(`Хотите вернуться на экран приветствия? Все ваши ответы будут потеряны`);
+
+    if (back) {
+      showIntro();
+    }
   };
 
   return headerView;
